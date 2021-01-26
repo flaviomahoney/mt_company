ba9528d8e7a6f1f115d09732abc0300ffb9e875a
flaviomahoney <flavio@mahoneyt.com>
2021-01-26 20:51:40 -0300

    Added routes and functions on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contato
+  end
+
+  def quem_somos
+  end
+
+  def produto
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/contato', to: 'pages#contato'
+  get '/quem_somos', to: 'pages#quem_somos'
+  get '/produto', to: 'pages#produto'
 end
