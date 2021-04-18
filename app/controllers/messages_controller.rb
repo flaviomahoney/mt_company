fbe2c7364ef43069b43eb331e3c3d27337191d6b
flaviomahoney <flavio@mahoneyt.com>
2021-04-18 12:34:09 -0300

    solved the new and create part of the project, it's not redirecting correctly but will come back for this later

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,20 @@ class MessagesController < ApplicationController
         @messages = Message.all
     end
 
+    def create
+        @message = Message.new(message_params)
+        if @message.save
+          flash[:success] = "message successfully created"
+          redirect_to @home
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+      private
+
+    def message_params
+        params.require(:message).permit(:nome, :sobrenome, :email, :descricao)
+    end
 end
