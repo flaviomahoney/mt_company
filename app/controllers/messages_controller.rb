71c18262775d055e7d0e30c19065920efc10e657
flaviomahoney <flavio@mahoneyt.com>
2021-04-18 12:29:40 -0300

    Trying to do the formulary to create a message in the home page, but still trying to understand de logic behind it

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,27 +4,4 @@ class MessagesController < ApplicationController
         @messages = Message.all
     end
 
-    def new
-        @message = Message.new
-    end
-
-    def create
-        @message = Message.find(params[:id])
-        @message = Message.new(message_params)
-        if @message.save
-          flash[:success] = "message successfully created"
-          redirect_to @message
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-
-    private
-
-    def message_params
-        params.require(:message).permit(:nome, :sobrenome, :email, :descricao)
-    end
-        
-
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def home
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.find(params[:id])
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = "message successfully created"
+      redirect_to @message
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:nome, :sobrenome, :email, :descricao)
   end
 end
