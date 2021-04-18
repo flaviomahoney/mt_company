aacd5ec18dd50e58f032d44bd09662439b883677
flaviomahoney <flavio@mahoneyt.com>
2021-04-18 12:20:42 -0300

    Added new and create to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,28 @@ class MessagesController < ApplicationController
     def index
         @messages = Message.all
     end
+
+    def new
+        @message = Message.new
+    end
+
+    def create
+        @message = Message.find(params[:id])
+        @message = Message.new(message_params)
+        if @message.save
+          flash[:success] = "message successfully created"
+          redirect_to @message
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:nome, :sobrenome, :email, :descricao)
+    end
+        
+
 end
